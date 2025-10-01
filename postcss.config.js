96485efa6002d13a2775d8074f5d963e07355c11
farhanshahriyar <farhan.abir9999@gmail.com>
2025-10-01 12:35:22 +0600

    fix: some minor changes

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    tailwindcss: {}, // tailwindcss configuration
+    autoprefixer: {}, // autoprefixer configuration
   },
 };
